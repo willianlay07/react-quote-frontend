9eb25128f35312a5e5e4cc7254ed0046d5e75aec
willianlay07 <willianlay07@gmail.com>
2023-09-21 19:25:40 +0800

    Modified Author Name

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -6,7 +6,7 @@ const Quote = ({ description, author }) => {
       <div className="quoteBlock">
         <h3 className="text-[15px]">{description}</h3>
         <br />
-        <p className="text-gray-600">{author}</p>
+        <p className="text-gray-600">- {author} -</p>
       </div>
     </>
   );
